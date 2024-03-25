d3d302997e9e445d8ec17c200c2da2f2a594d7ef
Yahya-Naji <95410696+Yahya-Naji@users.noreply.github.com>
2024-03-25 14:10:22 +0200

    edited

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import AppNavigation from './src/navigation/AppNavigation';
-import { NotesProvider } from './src/context/NotesContext';
+import AppNavigation from './src/navigations/AppNavigation';
+import { NotesProvider } from './src/context/NoteContext';
 
 export default function App() {
   return (
