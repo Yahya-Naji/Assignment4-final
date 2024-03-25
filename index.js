d7522c1ec50b00c44ccbd66971f9e59d0228555b
Yahya-Naji <95410696+Yahya-Naji@users.noreply.github.com>
2024-03-25 16:11:58 +0200

    edited

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import AppNavigation from './src/navigations/AppNavigation';
-import { NotesProvider } from './src/context/NoteContext';
+import { AppRegistry } from 'react-native';
+import App from './App';
+import { name as appName } from './app.json';
 
-export default function App() {
-  return (
-    <NotesProvider>
-      <NavigationContainer>
-        <AppNavigation />
-      </NavigationContainer>
-    </NotesProvider>
-  );
-}
+AppRegistry.registerComponent(appName, () => App);
